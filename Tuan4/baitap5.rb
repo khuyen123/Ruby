533b410ad6f6ef205f846f8cda3ecedfee8c8054
khuyen123 <khuyenphamno2@gmal.com>
2022-10-05 15:42:12 +0700

    edit

--- Tuan4/baitap5.rb
+++ Tuan4/baitap5.rb
@@ -1,56 +1,131 @@
 class Canbo
-    def initialize(hoten, tuoi, gioitinh, diachi)
-        @hoten = hoten
-        @tuoi = tuoi
-        @gioitinh = gioitinh
-        @diachi = diachi
+    def initialize
+    end 
+    def enter_infomation
+        print "Nhập tên : "
+        @name = gets.chomp
+        print "Nhập tuổi : "
+        @age = gets.to_i
+        print "Nhập giới tính 1(nam) 0(nữ) 2(khác) : "
+        @gender = gets.to_i
+        print "Nhập địa chỉ : "
+        @address = gets.chomp
     end
-    def hoten
-        @hoten
+    def info    
+        puts "Tên : #{@name}"
+        puts "Tuối : #{@age}"
+        if @gender == 1 
+            puts'Giới tính : Nam'
+        elsif @gender == 0
+            puts 'Giới Tính : Nữ'
+        else 
+            puts 'Giới Tính : Khác'
+        end
+        puts "Địa chỉ : #{@address}"
     end
-    def tuoi
-        @tuoi
-    end
-    def gioitinh
-        @gioitinh
-    end
-    def diachi
-        @diachi
-    end
-end
-#Xây dựng các lớp công nhân, kỹ sư, nhân viên kế thừa lớp cán bộ
+    attr_accessor :name, :age, :gender, :address
+end 
 class Congnhan < Canbo
-    attr_accessor : bac
-    def bac
-        @bac
+    def initialize
     end
+    attr_accessor :name, :age, :gender, :address, :bac
+    def enter_infomation
+        super
+        print "Nhập bậc : "
+        @bac = gets.chomp
+    end
+    def info 
+        super   
+        puts "Bậc : #{@bac}"
+    end
+
 end
 class Kysu < Canbo
-    attr_accessor : nganhdaotao
-    def nganhdaotao
-        @nganhdaotao
+    def initialize
     end
+    def enter_infomation
+        super
+        print "Nhập ngành đào tạo : "
+        @nganh = gets.chomp
+    end
+    def info 
+        super   
+        puts "Ngành : #{@nganh}"
+    end
+    attr_accessor :name, :age, :gender, :address, :nganh
 end
 class Nhanvien < Canbo
-    attr_accessor : congviec
-    def congviec
-        @congviec
+    def initialize
     end
+
+    def enter_infomation
+        super
+        print "Nhập công việc : "
+        @congviec = gets.chomp
+    end
+    def info 
+        super   
+        puts "Công việc : #{@congviec}"
+    end
+    attr_accessor :name, :age, :gender, :address, :congviec
+
 end
-class QLCB 
-    def themcanbo(danhsachcb[], Canbo canbo)
-       danhsachcb.push(canbo) 
+
+class QLCB
+    def initialize
     end
-    def timkiem(danhsachcb[], string ten)
-        for item in danhsachcb do
-            if item.hoten=ten 
-                return item
-            end
+
+    $canbos = []
+    
+    def addCanbo
+        puts"1. Nhâp công nhân :"
+        puts"2. Nhâp Ky su :"
+        puts"3. Nhập Nhân viên :"
+        print "Mơi bạn chon : "
+        choose = gets.to_i
+        if choose ==1 
+            canbo =Congnhan.new
+         
+        elsif choose == 2
+            canbo =Kysu.new
+        else 
+            canbo = Nhanvien.new
+        end
+        canbo.enter_infomation
+        $canbos << canbo       
+    end
+
+    def infomatinon 
+        puts "============Danh sách cán bộ là================"    
+        $canbos.each do |canbo|
+            puts canbo.info
         end
     end
-    def hienthi(danhsachcb[])
-        for item indanhsachcb do
-            puts "Họ tên: #{item.hoten}, tuổi: #{item.tuoi}, Giới tính: #{item.gioitinh}, Địa chỉ: #{item.diachi}" 
-        end
+
+end
+
+
+
+$continute = true
+while $continute
+
+    quanly = QLCB.new
+    puts "1. thêm mới cán bộ."
+    puts "2. Tìm kiếm theo tên."
+    puts "3. Hiện thị thông tin danh sách cán bộ ."
+    puts "4 Thoát khỏi chương trình."
+    print "Mời bạn chọn : "
+    choose = gets.to_i
+
+    case choose
+    when 1
+        quanly.addCanbo
+    when 2
+       puts "little child"
+    when 3
+        quanly.infomatinon
+    else
+       puts "Xin tạm biệt!"
+        $continute = false
     end
 end
